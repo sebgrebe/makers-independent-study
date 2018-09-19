3b8710d487dec91522302c57446dcbc5051b626e
Sebastian Becker <seb@Sebastians-MacBook-Pro.local>
2018-09-19 10:47:42 +0100

    week3, clients1, exercise 2.1

--- week3/clients1/client.rb
+++ week3/clients1/client.rb
@@ -2,9 +2,14 @@ require 'socket'
 
 socket = TCPSocket.new('localhost', 2345)
 
-puts socket.gets
-puts "Wow what a rude server... I'm going to say Sarah"
-socket.puts "Sarah"
-puts socket.gets
+loop do
+  puts socket.gets
+  puts "Wow what a rude server... I'm going to say Sarah"
+  socket.puts "Sarah"
+  puts socket.gets
+  puts "Another round? Enter y for yes"
+  input = gets.chomp
+  break if input != 'y'
+end
 
 socket.close
--- week3/clients1/server.rb
+++ week3/clients1/server.rb
@@ -4,8 +4,10 @@ server = TCPServer.new(2345)
 
 socket = server.accept
 
-socket.puts "HELLO CLIENT WHAT IS YOUR NAME?"
-name = socket.gets.chomp
-socket.puts "THAT'S A LOVELY NAME #{name}"
+loop do
+  socket.puts "HELLO CLIENT WHAT IS YOUR NAME?"
+  name = socket.gets.chomp
+  socket.puts "THAT'S A LOVELY NAME #{name}"
+end
 
 socket.close
