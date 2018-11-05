a08461719573e31223fb62d64efc12cef7565e41
Sebastian <sebbecker@gmx.net>
2018-11-05 12:26:44 +0000

    refactor

--- week10/timing/timing.java
+++ week10/timing/timing.java
@@ -3,40 +3,25 @@ import java.util.*;
 public class timing {
 
   public static void main(String[] args) {
-    int[] array = {1,2,3,4};
-    List<Integer> arrayList = new ArrayList<>();
-    for (int i = 1; i < 11; i++) {
-      arrayList.add(i);
-    }
+
+    List<Integer> arrayList = createList(100000);
+    int rounds = 1000000;
 
     //get time for getting last element of array
-    double diff = testGetLast(array);
+    double diff = testGetLast(arrayList, rounds);
     printTestResult(diff, "get last element of an array");
 
     //get time for reversing array
-    diff = testReverse(array);
+    diff = testReverse(arrayList, 1000);
     printTestResult(diff, "reverse array");
 
     //get time for shuffling
-    diff = testShuffle(arrayList);
+    diff = testShuffle(arrayList, 1000);
     printTestResult(diff, "shuffle array");
 
     //get time for sorting
-    diff = testShuffle(arrayList);
+    diff = testShuffle(arrayList, 1000);
     printTestResult(diff, "sort array");
-  }
-
-  public static double testGetLast(int[] testArray) {
-    double startTime = System.currentTimeMillis();
-    double endTime = 0;
-    int rounds = 1000000;
-    for (int i = 0; i < rounds; i++) {
-      int last = testArray[testArray.length - 1];
-    }
-    endTime = System.currentTimeMillis();
-    // System.out.println(startTime);
-    double diff = (endTime - startTime) / rounds ;
-    return diff;
   }
 
   public static void printTestResult(double result, String operation) {
@@ -46,25 +31,40 @@ public class timing {
     );
   }
 
-  public static double testReverse(int[] testArray) {
+  public static List<Integer> createList(int size) {
+    List<Integer> arrayList = new ArrayList<Integer>();
+    for (int i = 1; i < size; i++) {
+      arrayList.add(i);
+    }
+    return arrayList;
+  }
+
+  public static double testGetLast(List testArrayList, int rounds) {
     double startTime = System.currentTimeMillis();
     double endTime = 0;
-    int[] reversedArray = new int[testArray.length];
-    int rounds = 1000000;
+    for (int i = 0; i < rounds; i++) {
+      Object last = testArrayList.get(testArrayList.size() - 1);
+    }
+    endTime = System.currentTimeMillis();
+    // System.out.println(startTime);
+    double diff = (endTime - startTime) / rounds ;
+    return diff;
+  }
+
+  public static double testReverse(List testArrayList, int rounds) {
+    double startTime = System.currentTimeMillis();
+    double endTime = 0;
     for (int j=0; j < rounds; j++) {
-      for (int i=0; i < testArray.length / 2; i++) {
-        reversedArray[i] = testArray[testArray.length - (i + 1)];
-      }
+      Collections.reverse(testArrayList);
     }
     endTime = System.currentTimeMillis();
     double diff = (endTime - startTime) / rounds;
     return diff;
   }
 
-  public static double testShuffle(List testArrayList) {
+  public static double testShuffle(List testArrayList, int rounds) {
     double startTime = System.currentTimeMillis();
     double endTime = 0;
-    int rounds = 1000000;
     for (int j=0; j < rounds; j++) {
       Collections.shuffle(testArrayList);
     }
@@ -73,10 +73,9 @@ public class timing {
     return diff;
   }
 
-  public static double testSort(List testArrayList) {
+  public static double testSort(List testArrayList, int rounds) {
     double startTime = System.currentTimeMillis();
     double endTime = 0;
-    int rounds = 1000000;
     Collections.shuffle(testArrayList);
     for (int j=0; j < rounds; j++) {
       Collections.sort(testArrayList);
